adf5b0d389c204e8f77e428c27f5f0215eb04f86
fntwin <gabdettofntwin@gmail.com>
2020-07-19 14:23:07 +0200

    changes + logger

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name='GPGO',
-    version='0.1.0',
+    version='0.1.1',
     author='Cristian Gabellini',
     packages=find_packages(),
     url='https://github.com/FNTwin/GPGO',
